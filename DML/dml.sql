61624cb900f5662e03db2a5a4d8b390b0f2fa636
ElidallanaCristancho <cristanchodayana062017@gmail.com>
2024-11-25 15:24:57 -0500

    feat: :sparkles: :Inserciones

--- DML/dml.sql
+++ DML/dml.sql
@@ -374,3 +374,885 @@ INSERT INTO Ventas (id_cliente, id_empleado, fecha, total) VALUES
 (48, 5, '2024-02-17', 420.00),
 (49, 2, '2024-02-18', 300.60),
 (50, 1, '2024-02-19', 180.50);
+
+
+INSERT INTO DetalleVentas (id_venta, id_producto, cantidad, precio_unitario) VALUES
+
+(1, 1, 100.00, 0.90),
+
+(1, 2, 50.00, 1.10),
+
+(2, 3, 150.00, 1.20),
+
+(2, 4, 100.00, 1.50),
+
+(3, 5, 200.00, 0.80),
+
+(3, 6, 80.00, 1.20),
+
+(4, 7, 120.00, 1.50),
+
+(4, 8, 90.00, 1.10),
+
+(5, 9, 50.00, 1.20),
+
+(5, 10, 100.00, 1.80),
+
+(6, 11, 200.00, 2.00),
+
+(6, 12, 50.00, 1.50),
+
+(7, 13, 80.00, 1.00),
+
+(7, 14, 100.00, 2.50),
+
+(8, 15, 150.00, 2.50),
+
+(8, 16, 50.00, 2.00),
+
+(9, 17, 60.00, 1.50),
+
+(9, 18, 40.00, 2.50),
+
+(10, 19, 100.00, 1.80),
+
+(10, 20, 150.00, 2.50),
+
+(11, 21, 100.00, 1.20),
+
+(11, 22, 100.00, 1.50),
+
+(12, 23, 120.00, 0.70),
+
+(12, 24, 80.00, 1.00),
+
+(13, 25, 60.00, 3.00),
+
+(13, 26, 80.00, 2.50),
+
+(14, 27, 90.00, 3.00),
+
+(14, 28, 100.00, 2.00),
+
+(15, 29, 100.00, 1.00),
+
+(15, 30, 100.00, 1.50),
+
+(16, 31, 120.00, 0.80),
+
+(16, 32, 150.00, 1.20),
+
+(17, 33, 80.00, 1.50),
+
+(17, 34, 60.00, 2.00),
+
+(18, 35, 100.00, 1.80),
+
+(18, 36, 80.00, 1.60),
+
+(19, 37, 150.00, 1.20),
+
+(19, 38, 100.00, 1.50),
+
+(20, 39, 80.00, 3.00),
+
+(20, 40, 100.00, 2.50),
+
+(21, 41, 120.00, 2.00),
+
+(21, 42, 80.00, 1.50),
+
+(22, 43, 200.00, 2.50),
+
+(22, 44, 100.00, 1.80),
+
+(23, 45, 100.00, 3.00),
+
+(23, 46, 80.00, 2.50),
+
+(24, 47, 50.00, 2.50),
+
+(24, 48, 70.00, 1.80),
+
+(25, 49, 80.00, 2.00),
+
+(25, 50, 120.00, 1.50);
+
+INSERT INTO Proveedores (nombre, telefono, correo, direccion)
+
+VALUES
+
+('AgroFinca S.A.S.', '314-1234567', 'contacto@agrofinca.com', 'Carrera 5 # 10-25, Valle del Cauca'),
+
+('Finca El Cielo', '315-2345678', 'ventas@fincaelcielo.com', 'Vereda La Esperanza, Boyacá'),
+
+('Hortalizas Los Andes', '317-3456789', 'info@hortalizaslosandes.com', 'Km 12 vía La Vega, Cundinamarca'),
+
+('Semillas del Valle', '310-4567890', 'contacto@semillasdelvalle.com', 'Sector El Roble, Santander'),
+
+('Finca La Toscana', '318-5678901', 'toscana@finca.com', 'Vereda Los Naranjos, Quindío'),
+
+('AgroPlanta S.A.', '319-6789012', 'ventas@agroplanta.com', 'Carrera 7 # 15-30, Nariño'),
+
+('La Sabana Verde', '321-7890123', 'sabana@verde.com', 'Km 8 vía Cúcuta, Norte de Santander'),
+
+('Frutos de la Tierra', '322-8901234', 'frutos@tierra.com', 'Finca El Palmar, Meta'),
+
+('AgroFuturo', '323-9012345', 'agrofuturo@agro.com', 'Sector El Parque, Huila'),
+
+('Finca Santa Teresa', '324-0123456', 'santateresa@finca.com', 'Vereda La Loma, Antioquia'),
+
+('Productos del Campo', '325-1234567', 'productos@campo.com', 'Vereda San Juan, Tolima'),
+
+('Finca Las Delicias', '326-2345678', 'info@lasdeliciasfinca.com', 'Carrera 12 # 8-15, Cauca'),
+
+('AgroFrutos S.A.S.', '327-3456789', 'contacto@agrofrutos.com', 'Sector El Cerrito, Cesar'),
+
+('Viveros El Edén', '328-4567890', 'edenviveros@agro.com', 'Km 4 vía Barranquilla, Atlántico'),
+
+('Finca Los Pinos', '329-5678901', 'lospinos@finca.com', 'Vereda Las Palmas, Valle del Cauca'),
+
+('Cosechas del Sol', '330-6789012', 'sol@cosechas.com', 'Carrera 5 # 11-20, Risaralda'),
+
+('Finca La Esperanza', '331-7890123', 'esperanza@finca.com', 'Sector Santa Marta, Magdalena'),
+
+('Semillas Agroverde', '332-8901234', 'agroverde@semillas.com', 'Vereda El Zapato, Cundinamarca'),
+
+('Productos del Campo Verde', '333-9012345', 'campo@verde.com', 'Finca El Edén, Casanare'),
+
+('AgroCosechas', '334-0123456', 'contacto@agrocosechas.com', 'Carrera 9 # 20-12, Bolívar'),
+
+('Hortalizas El Campo', '335-1234567', 'hortalizas@campo.com', 'Km 5 vía Ibagué, Tolima'),
+
+('Finca El Paraíso', '336-2345678', 'paraiso@finca.com', 'Vereda El Paraíso, Cundinamarca'),
+
+('La Granja Agrícola', '337-3456789', 'granja@agricola.com', 'Sector Loma Linda, Valle del Cauca'),
+
+('AgroTierra', '338-4567890', 'tierragro@agro.com', 'Vereda Los Andes, Quindío'),
+
+('Finca Los Jardines', '339-5678901', 'jardines@finca.com', 'Km 3 vía Medellín, Antioquia'),
+
+('Viveros y Cultivos Los Andes', '340-6789012', 'viveros@andes.com', 'Vereda Los Cedros, Boyacá'),
+
+('Finca La Roca', '341-7890123', 'laroca@finca.com', 'Carrera 6 # 14-40, Nariño'),
+
+('Cultivos el Sol', '342-8901234', 'solcultivos@campo.com', 'Finca El Sol, Huila'),
+
+('Finca La Granja', '343-9012345', 'granja@finca.com', 'Sector La Loma, Tolima'),
+
+('Semillas y Sembrios', '344-0123456', 'semillas@sembrios.com', 'Carrera 2 # 16-30, Sucre'),
+
+('Finca El Oasis', '345-1234567', 'el@oasis.com', 'Km 10 vía Tuluá, Valle del Cauca'),
+
+('AgroFinca El Valle', '346-2345678', 'contacto@agrofinca.com', 'Vereda San Antonio, Santander'),
+
+('Finca La Bella', '347-3456789', 'bella@finca.com', 'Vereda La Calera, Cundinamarca'),
+
+('Hortalizas Las Vegas', '348-4567890', 'vegas@hortalizas.com', 'Carrera 8 # 12-45, Atlántico'),
+
+('AgroLínea', '349-5678901', 'contacto@agrolínea.com', 'Sector El Paraíso, Casanare'),
+
+('Finca Los Cedros', '350-6789012', 'cedros@finca.com', 'Finca Los Cedros, Cundinamarca'),
+
+('Finca Santa Catalina', '351-7890123', 'catalina@finca.com', 'Km 4 vía Tunja, Boyacá'),
+
+('Finca El Encanto', '352-8901234', 'encanto@finca.com', 'Sector El Campo, Risaralda'),
+
+('Semillas El Paraíso', '353-9012345', 'semillas@paraiso.com', 'Vereda El Mirador, Antioquia'),
+
+('Productos La Bella', '354-0123456', 'productos@bella.com', 'Finca La Bella, Tolima'),
+
+('AgroValle', '355-1234567', 'valle@agro.com', 'Carrera 3 # 11-70, Norte de Santander'),
+
+('La Tierra Prometida', '356-2345678', 'tierra@prometida.com', 'Vereda La Estrella, Meta'),
+
+('Finca Los Limones', '357-3456789', 'limones@finca.com', 'Km 12 vía Yopal, Casanare'),
+
+('AgroCultivos La Luna', '358-4567890', 'luna@agrocultivos.com', 'Finca La Luna, Quindío'),
+
+('Finca San Pedro', '359-5678901', 'sanpedro@finca.com', 'Vereda San Pedro, Santander'),
+
+('Cosecha Tropical', '360-6789012', 'tropical@cosecha.com', 'Carrera 4 # 14-30, Bolívar'),
+
+('Finca El Corral', '361-7890123', 'corral@finca.com', 'Km 7 vía Sogamoso, Boyacá'),
+
+('Productos y Semillas El Sol', '362-8901234', 'sol@semillas.com', 'Finca El Sol, Huila'),
+
+('AgroCosecha El Edén', '363-9012345', 'edén@agrocosecha.com', 'Vereda El Edén, Nariño'),
+
+('Finca Las Palmas', '364-0123456', 'palmas@finca.com', 'Carrera 6 # 22-18, Córdoba'),
+
+('Semillas del Sur', '365-1234567', 'sur@semillas.com', 'Finca El Sur, Cundinamarca'),
+
+('Finca El Laurel', '366-2345678', 'laurel@finca.com', 'Km 9 vía Tuluá, Valle del Cauca'),
+
+('AgroFinca Los Pinos', '367-3456789', 'lospinos@agrofinca.com', 'Vereda Los Naranjos, Santander'),
+
+('Finca El Refugio', '368-4567890', 'refugio@finca.com', 'Vereda San Juan, Tolima'),
+
+('La Cosecha del Valle', '369-5678901', 'cosechavalley@campo.com', 'Finca El Valle, Boyacá'),
+
+('Finca San Antonio', '370-6789012', 'sanantonio@finca.com', 'Vereda El Carmen, Meta');
+
+INSERT INTO Compras (id_proveedor, id_empleado, fecha, total)
+
+VALUES
+
+(1, 1, '2024-11-01', 1500.50),
+
+(2, 2, '2024-11-02', 3200.00),
+
+(3, 3, '2024-11-03', 4500.75),
+
+(4, 4, '2024-11-04', 7800.25),
+
+(5, 5, '2024-11-05', 6200.10),
+
+(6, 6, '2024-11-06', 11200.40),
+
+(7, 7, '2024-11-07', 3500.00),
+
+(8, 8, '2024-11-08', 2900.90),
+
+(9, 9, '2024-11-09', 5800.65),
+
+(10, 10, '2024-11-10', 6400.50),
+
+(11, 11, '2024-11-11', 2200.00),
+
+(12, 12, '2024-11-12', 7700.25),
+
+(13, 13, '2024-11-13', 8000.30),
+
+(14, 14, '2024-11-14', 5200.00),
+
+(15, 15, '2024-11-15', 3700.45),
+
+(16, 16, '2024-11-16', 8900.00),
+
+(17, 17, '2024-11-17', 6400.20),
+
+(18, 18, '2024-11-18', 5400.35),
+
+(19, 19, '2024-11-19', 10000.50),
+
+(20, 20, '2024-11-20', 6800.90),
+
+(21, 21, '2024-11-21', 9300.75),
+
+(22, 22, '2024-11-22', 7600.15),
+
+(23, 23, '2024-11-23', 11000.00),
+
+(24, 24, '2024-11-24', 8500.60),
+
+(25, 25, '2024-11-25', 4300.25),
+
+(26, 26, '2024-11-26', 5100.35),
+
+(27, 27, '2024-11-27', 5600.50),
+
+(28, 28, '2024-11-28', 3300.45),
+
+(29, 29, '2024-11-29', 6200.00),
+
+(30, 30, '2024-11-30', 7700.35),
+
+(31, 31, '2024-12-01', 6400.90),
+
+(32, 32, '2024-12-02', 4500.75),
+
+(33, 33, '2024-12-03', 8500.00),
+
+(34, 34, '2024-12-04', 4200.25),
+
+(35, 35, '2024-12-05', 5300.45),
+
+(36, 36, '2024-12-06', 6100.10),
+
+(37, 37, '2024-12-07', 3400.00),
+
+(38, 38, '2024-12-08', 7800.00),
+
+(39, 39, '2024-12-09', 9200.30),
+
+(40, 40, '2024-12-10', 7700.50),
+
+(41, 41, '2024-12-11', 9500.60),
+
+(42, 42, '2024-12-12', 6800.00),
+
+(43, 43, '2024-12-13', 8900.70),
+
+(44, 44, '2024-12-14', 5200.00),
+
+(45, 45, '2024-12-15', 6700.50),
+
+(46, 46, '2024-12-16', 7200.00),
+
+(47, 47, '2024-12-17', 8100.90),
+
+(48, 48, '2024-12-18', 6000.50),
+
+(49, 49, '2024-12-19', 8000.10),
+
+(50, 50, '2024-12-20', 9300.35);
+
+INSERT INTO DetalleCompras (id_compra, id_producto, cantidad, precio_unitario)
+
+VALUES
+
+(1, 1, 50, 30.00),
+
+(1, 2, 30, 45.00),
+
+(1, 3, 20, 70.00),
+
+(2, 1, 100, 35.00),
+
+(2, 4, 15, 100.00),
+
+(2, 5, 40, 60.00),
+
+(3, 3, 25, 70.00),
+
+(3, 1, 60, 32.50),
+
+(3, 6, 20, 80.00),
+
+(4, 2, 30, 45.00),
+
+(4, 5, 50, 55.00),
+
+(4, 7, 10, 150.00),
+
+(5, 3, 30, 70.00),
+
+(5, 4, 25, 110.00),
+
+(5, 6, 40, 65.00),
+
+(6, 1, 50, 30.00),
+
+(6, 3, 20, 75.00),
+
+(6, 2, 40, 40.00),
+
+(7, 4, 15, 100.00),
+
+(7, 7, 30, 120.00),
+
+(7, 5, 60, 60.00),
+
+(8, 3, 20, 70.00),
+
+(8, 6, 25, 85.00),
+
+(8, 1, 70, 30.50),
+
+(9, 4, 30, 95.00),
+
+(9, 1, 50, 32.00),
+
+(9, 3, 40, 68.00),
+
+(10, 5, 50, 55.00),
+
+(10, 2, 30, 45.00),
+
+(10, 6, 20, 80.00),
+
+(11, 7, 10, 120.00),
+
+(11, 3, 30, 65.00),
+
+(11, 1, 50, 33.50),
+
+(12, 4, 15, 110.00),
+
+(12, 2, 25, 45.00),
+
+(12, 5, 50, 60.00),
+
+(13, 3, 25, 70.00),
+
+(13, 6, 20, 85.00),
+
+(13, 1, 50, 31.50),
+
+(14, 7, 10, 125.00),
+
+(14, 1, 50, 32.00),
+
+(14, 5, 20, 55.00),
+
+(15, 4, 15, 100.00),
+
+(15, 2, 30, 40.00),
+
+(15, 6, 40, 65.00),
+
+(16, 7, 20, 120.00),
+
+(16, 1, 70, 32.00),
+
+(16, 3, 25, 75.00),
+
+(17, 2, 50, 45.00),
+
+(17, 5, 30, 60.00),
+
+(17, 6, 20, 85.00);
+
+INSERT INTO Produccion (id_producto, fecha, cantidad) VALUES
+
+(1, '2024-01-01', 1000.00),
+
+(2, '2024-01-02', 800.00),
+
+(3, '2024-01-03', 1200.00),
+
+(4, '2024-01-04', 900.00),
+
+(5, '2024-01-05', 1100.00),
+
+(6, '2024-01-06', 1300.00),
+
+(7, '2024-01-07', 1000.00),
+
+(8, '2024-01-08', 950.00),
+
+(9, '2024-01-09', 1100.00),
+
+(10, '2024-01-10', 1200.00),
+
+(11, '2024-01-11', 1000.00),
+
+(12, '2024-01-12', 900.00),
+
+(13, '2024-01-13', 800.00),
+
+(14, '2024-01-14', 950.00),
+
+(15, '2024-01-15', 1000.00),
+
+(16, '2024-01-16', 1200.00),
+
+(17, '2024-01-17', 1300.00),
+
+(18, '2024-01-18', 1100.00),
+
+(19, '2024-01-19', 1050.00),
+
+(20, '2024-01-20', 1150.00),
+
+(21, '2024-01-21', 1000.00),
+
+(22, '2024-01-22', 1100.00),
+
+(23, '2024-01-23', 1200.00),
+
+(24, '2024-01-24', 1000.00),
+
+(25, '2024-01-25', 900.00),
+
+(26, '2024-01-26', 1100.00),
+
+(27, '2024-01-27', 1200.00),
+
+(28, '2024-01-28', 1300.00),
+
+(29, '2024-01-29', 1100.00),
+
+(30, '2024-01-30', 1000.00),
+
+(31, '2024-02-01', 800.00),
+
+(32, '2024-02-02', 950.00),
+
+(33, '2024-02-03', 1000.00),
+
+(34, '2024-02-04', 900.00),
+
+(35, '2024-02-05', 1050.00),
+
+(36, '2024-02-06', 1200.00),
+
+(37, '2024-02-07', 1300.00),
+
+(38, '2024-02-08', 950.00),
+
+(39, '2024-02-09', 1100.00),
+
+(40, '2024-02-10', 1000.00),
+
+(41, '2024-02-11', 1050.00),
+
+(42, '2024-02-12', 1150.00),
+
+(43, '2024-02-13', 1000.00),
+
+(44, '2024-02-14', 900.00),
+
+(45, '2024-02-15', 1100.00),
+
+(46, '2024-02-16', 1200.00),
+
+(47, '2024-02-17', 1300.00),
+
+(48, '2024-02-18', 1000.00),
+
+(49, '2024-02-19', 1050.00),
+
+(50, '2024-02-20', 1100.00);
+
+INSERT INTO RelacionProveedoresProductos (id_proveedor, id_producto) VALUES
+
+(1, 1),
+
+(1, 2),
+
+(2, 3),
+
+(2, 4),
+
+(3, 5),
+
+(3, 6),
+
+(4, 7),
+
+(4, 8),
+
+(5, 9),
+
+(5, 10),
+
+(6, 11),
+
+(6, 12),
+
+(7, 13),
+
+(7, 14),
+
+(8, 15),
+
+(8, 16),
+
+(9, 17),
+
+(9, 18),
+
+(10, 19),
+
+(10, 20),
+
+(11, 21),
+
+(11, 22),
+
+(12, 23),
+
+(12, 24),
+
+(13, 25),
+
+(13, 26),
+
+(14, 27),
+
+(14, 28),
+
+(15, 29),
+
+(15, 30),
+
+(16, 31),
+
+(16, 32),
+
+(17, 33),
+
+(17, 34),
+
+(18, 35),
+
+(18, 36),
+
+(19, 37),
+
+(19, 38),
+
+(20, 39),
+
+(20, 40),
+
+(21, 41),
+
+(21, 42),
+
+(22, 43),
+
+(22, 44),
+
+(23, 45),
+
+(23, 46),
+
+(24, 47),
+
+(24, 48),
+
+(25, 49),
+
+(25, 50);
+
+INSERT INTO RelacionClientesProductos (id_cliente, id_producto) VALUES
+
+(1, 1),
+
+(1, 3),
+
+(1, 5),
+
+(2, 2),
+
+(2, 6),
+
+(2, 8),
+
+(3, 4),
+
+(3, 7),
+
+(3, 9),
+
+(4, 10),
+
+(4, 12),
+
+(4, 14),
+
+(5, 13),
+
+(5, 15),
+
+(5, 17),
+
+(6, 18),
+
+(6, 19),
+
+(6, 20),
+
+(7, 21),
+
+(7, 22),
+
+(7, 23),
+
+(8, 24),
+
+(8, 25),
+
+(8, 26),
+
+(9, 27),
+
+(9, 28),
+
+(9, 29),
+
+(10, 30),
+
+(10, 31),
+
+(10, 32),
+
+(11, 33),
+
+(11, 34),
+
+(11, 35),
+
+(12, 36),
+
+(12, 37),
+
+(12, 38),
+
+(13, 39),
+
+(13, 40),
+
+(13, 41),
+
+(14, 42),
+
+(14, 43),
+
+(14, 44),
+
+(15, 45),
+
+(15, 46),
+
+(15, 47),
+
+(16, 48),
+
+(16, 49),
+
+(16, 50),
+
+(17, 12),
+
+(18, 19);
+
+INSERT INTO AsignacionTareas (id_empleado, descripcion, fecha_asignacion, fecha_limite, estado) VALUES
+
+(1, 'Cosecha de maíz en el campo 1', '2024-11-01', '2024-11-05', 'Pendiente'),
+
+(1, 'Revisión de cultivos de papa', '2024-11-03', '2024-11-07', 'Pendiente'),
+
+(2, 'Siembra de frijoles en el campo 2', '2024-11-02', '2024-11-08', 'En Progreso'),
+
+(2, 'Mantenimiento de la maquinaria', '2024-11-04', '2024-11-06', 'Pendiente'),
+
+(3, 'Recolección de frutos de guanábana', '2024-11-01', '2024-11-10', 'Completado'),
+
+(3, 'Poda de los árboles de mango', '2024-11-05', '2024-11-12', 'Pendiente'),
+
+(4, 'Preparación del terreno para siembra', '2024-11-06', '2024-11-09', 'En Progreso'),
+
+(4, 'Revisión de las condiciones del riego', '2024-11-07', '2024-11-09', 'Pendiente'),
+
+(5, 'Siembra de zanahoria', '2024-11-03', '2024-11-10', 'Pendiente'),
+
+(5, 'Aplicación de fertilizantes en campo de arroz', '2024-11-01', '2024-11-04', 'Completado'),
+
+(6, 'Mantenimiento del sistema de riego', '2024-11-04', '2024-11-08', 'Pendiente'),
+
+(6, 'Recolección de tomates', '2024-11-07', '2024-11-12', 'Pendiente'),
+
+(7, 'Cosecha de café en la finca 3', '2024-11-05', '2024-11-11', 'En Progreso'),
+
+(7, 'Poda de los árboles de cítricos', '2024-11-02', '2024-11-05', 'Completado'),
+
+(8, 'Revisión de inventarios de productos', '2024-11-06', '2024-11-08', 'Pendiente'),
+
+(8, 'Entrega de productos a clientes', '2024-11-07', '2024-11-10', 'Pendiente'),
+
+(9, 'Recogida de residuos agrícolas', '2024-11-02', '2024-11-04', 'Completado'),
+
+(9, 'Siembra de alfalfa', '2024-11-01', '2024-11-05', 'Pendiente'),
+
+(10, 'Aplicación de pesticidas en el campo 5', '2024-11-05', '2024-11-10', 'Pendiente'),
+
+(10, 'Limpieza de las instalaciones', '2024-11-03', '2024-11-04', 'Completado'),
+
+(11, 'Cosecha de plátanos', '2024-11-08', '2024-11-14', 'Pendiente'),
+
+(11, 'Mantenimiento de los almacenes', '2024-11-09', '2024-11-11', 'Pendiente'),
+
+(12, 'Revisión de condiciones de almacenamiento', '2024-11-02', '2024-11-04', 'Completado'),
+
+(12, 'Cosecha de guayaba', '2024-11-03', '2024-11-07', 'En Progreso'),
+
+(13, 'Siembra de maíz forrajero', '2024-11-01', '2024-11-05', 'Pendiente'),
+
+(13, 'Revisión de la maquinaria de cosecha', '2024-11-07', '2024-11-09', 'En Progreso'),
+
+(14, 'Recolección de uvas', '2024-11-04', '2024-11-09', 'Pendiente'),
+
+(14, 'Cosecha de pepino', '2024-11-06', '2024-11-10', 'Pendiente'),
+
+(15, 'Control de maleza en el campo 6', '2024-11-02', '2024-11-06', 'En Progreso'),
+
+(15, 'Poda de árboles de café', '2024-11-05', '2024-11-08', 'Pendiente'),
+
+(16, 'Siembra de pepino', '2024-11-01', '2024-11-04', 'Pendiente'),
+
+(16, 'Preparación de las áreas para siembra de arroz', '2024-11-08', '2024-11-11', 'Pendiente'),
+
+(17, 'Aplicación de herbicidas', '2024-11-06', '2024-11-09', 'Pendiente'),
+
+(17, 'Revisión de equipos de riego', '2024-11-07', '2024-11-09', 'En Progreso'),
+
+(18, 'Cosecha de fresa', '2024-11-05', '2024-11-10', 'Completado'),
+
+(18, 'Mantenimiento del sistema eléctrico de la finca', '2024-11-02', '2024-11-03', 'Completado'),
+
+(19, 'Siembra de cebada', '2024-11-03', '2024-11-07', 'Pendiente'),
+
+(19, 'Recolección de remolacha', '2024-11-04', '2024-11-08', 'Pendiente'),
+
+(20, 'Control de plagas en campo 2', '2024-11-01', '2024-11-03', 'Completado'),
+
+(20, 'Poda de árboles frutales', '2024-11-06', '2024-11-08', 'Pendiente'),
+
+(21, 'Siembra de arroz en campo 7', '2024-11-03', '2024-11-06', 'Pendiente'),
+
+(21, 'Cosecha de habas', '2024-11-04', '2024-11-07', 'Pendiente'),
+
+(22, 'Mantenimiento de los tanques de riego', '2024-11-07', '2024-11-09', 'En Progreso'),
+
+(22, 'Siembra de alfalfa en campo 3', '2024-11-02', '2024-11-06', 'Pendiente'),
+
+(23, 'Recolección de frambuesa', '2024-11-05', '2024-11-10', 'Pendiente'),
+
+(23, 'Siembra de pimientos', '2024-11-06', '2024-11-09', 'Pendiente'),
+
+(24, 'Control de maleza en campo de yuca', '2024-11-03', '2024-11-06', 'Pendiente'),
+
+(24, 'Revisión de condiciones de cultivo de zanahoria', '2024-11-04', '2024-11-07', 'Pendiente'),
+
+(25, 'Cosecha de caña de azúcar', '2024-11-01', '2024-11-04', 'Completado'),
+
+(25, 'Limpieza de áreas de cultivo', '2024-11-05', '2024-11-08', 'Pendiente'),
+
+(26, 'Recolección de guanábanas', '2024-11-02', '2024-11-05', 'En Progreso'),
+
+(26, 'Siembra de frijoles en el campo 2', '2024-11-03', '2024-11-06', 'Pendiente'),
+
+(27, 'Poda de hortalizas', '2024-11-06', '2024-11-09', 'Pendiente'),
+
+(27, 'Revisión de inventarios de pesticidas', '2024-11-04', '2024-11-05', 'Completado'),
+
+(28, 'Siembra de cebollas', '2024-11-01', '2024-11-04', 'Pendiente'),
+
+(28, 'Revisión de campos de tomate', '2024-11-02', '2024-11-05', 'En Progreso'),
+
+(29, 'Recolección de guayabas', '2024-11-07', '2024-11-10', 'Pendiente'),
+
+(29, 'Siembra de yuca', '2024-11-03', '2024-11-06', 'Pendiente'),
+
+(30, 'Mantenimiento del tractor', '2024-11-01', '2024-11-04', 'Pendiente'),
+
+(30, 'Revisión de equipos de riego en campo 4', '2024-11-05', '2024-11-08', 'Pendiente'),
+
+(31, 'Cosecha de zanahorias', '2024-11-06', '2024-11-09', 'Pendiente'),
+
+(31, 'Poda de cultivos de hortalizas', '2024-11-02', '2024-11-05', 'En Progreso'),
+
+(32, 'Revisión de plagas en campo de hortalizas', '2024-11-03', '2024-11-06', 'Pendiente'),
+
+(32, 'Recolección de aguacates', '2024-11-04', '2024-11-07', 'Pendiente'),
+
+(33, 'Mantenimiento de los cultivos de papa', '2024-11-01', '2024-11-04', 'Pendiente'),
+
+(33, 'Siembra de pepinos en campo 5', '2024-11-07', '2024-11-10', 'Pendiente'),
+
+(34, 'Control de plagas en campo 3', '2024-11-02', '2024-11-04', 'En Progreso'),
+
+(34, 'Cosecha de pimientos', '2024-11-05', '2024-11-08', 'Pendiente'),
+
+(35, 'Revisión de la maquinaria de siembra', '2024-11-03', '2024-11-06', 'Pendiente'),
+
+(35, 'Poda de árboles de cítricos', '2024-11-04', '2024-11-07', 'Completado'),
+
+(36, 'Siembra de maíz en campo 1', '2024-11-01', '2024-11-05', 'Pendiente'),
+
+(36, 'Revisión de cultivos de guanábana', '2024-11-05', '2024-11-08', 'Pendiente');
+
